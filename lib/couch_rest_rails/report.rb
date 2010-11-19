f1a7d22508cb4032dff6c2a2afccbebf6ceda84e
Fedor Fomenko <ffomenko@yahoo.co.uk>
2010-11-19 17:30:01 +0000

    Some fixes

--- lib/couch_rest_rails/report.rb
+++ lib/couch_rest_rails/report.rb
@@ -1,10 +1,8 @@
 module CouchRestRails
   module Report
-    def self.included(klass)
-      class << klass
-
+    def self.included(base)
+      class << base
         attr_accessor :database
-
         def use_database(db)
           db = [COUCHDB_CONFIG[:db_prefix], db.to_s, COUCHDB_CONFIG[:db_suffix]].join
           self.database = COUCHDB_SERVER.database(db)
